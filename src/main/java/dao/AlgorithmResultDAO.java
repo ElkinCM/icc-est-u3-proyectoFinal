161bedb1fe9b0c20c157c83d356bec06b6d77dda
Mateo <mateomorejon993@gmail.com>
2025-07-28 22:24:09 -0500

    feat:Implementacion del DAO

---
+++ src/main/java/dao/AlgorithmResultDAO.java
@@ -0,0 +1,12 @@
+package dao;
+
+import models.AlgorithmResult;
+
+import java.util.List;
+
+
+public interface AlgorithmResultDAO {
+    void guardarResultado(AlgorithmResult resultado);
+    List<AlgorithmResult> obtenerResultados();
+    void limpiarResultados();
+}
