a04c7f0e73fad9dd80352b9c175330847a7274bd
CMSebas <rafaselm918@gmail.com>
2025-07-28 23:42:34 -0500

    Feat:Creacion de la interfaz final que une las ventanas de mostrar resultados y de la grafica que muestra el recorrrido de los laberintos junto las demas ventanas

---
+++ src/main/java/MazeApp.java
@@ -0,0 +1,33 @@
+import controllers.MazeController;
+import dao.AlgorithmResultDAO;
+import dao.Impl.AlgorithmResultDAOFile;
+import solver.solverImpl.*;
+
+import views.MazeFrame;
+import views.MazePanel;
+
+import javax.swing.*;
+
+public class MazeApp {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {      
+            AlgorithmResultDAO resultDAO = new AlgorithmResultDAOFile();
+
+            MazePanel panel = new MazePanel();
+
+            MazeController controller = new MazeController(resultDAO);
+            controller.setMatriz(panel.getCells());
+            panel.setController(controller);
+
+            controller.registrarAlgoritmo("RECURSIVO", new MazeSolverRecursivo());
+            controller.registrarAlgoritmo("RECURSIVO COMPLETO", new MazeSolverRecursivoCompleto());
+            controller.registrarAlgoritmo("RECURSIVO COMPLETO BT", new MazeSolverRecursivoCompletoBT());
+            controller.registrarAlgoritmo("DFS", new MazeSolverDFS());
+            controller.registrarAlgoritmo("BFS", new MazeSolverBFS());
+            controller.registrarAlgoritmo("Bactracking", new MazeSolverRecursivo());
+
+            new MazeFrame(controller, panel);
+        });
+    }
+}
